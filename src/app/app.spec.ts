27f7842b9b899266d153dc21c90a003afd2517ee
sayheylaura <lauraferrandof@gmail.com>
2025-07-22 23:23:57 +0200

    test: fix failing tests

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,26 +1,30 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, provideRouter } from '@angular/router';
+
 import { App } from './app';
-import { HeaderComponent } from './layout/header/header.component';
+import { routes } from './app.routes';
 import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './layout/header/header.component';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App, HeaderComponent, FooterComponent, RouterOutlet],
+      providers: [provideRouter(routes)],
     }).compileComponents();
   });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(App);
     const app = fixture.componentInstance;
+
     expect(app).toBeTruthy();
   });
 
   it('should render header, main content, and footer', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
+    const compiled = fixture.nativeElement;
 
     expect(compiled.querySelector('app-header')).toBeTruthy();
     expect(compiled.querySelector('main')).toBeTruthy();
