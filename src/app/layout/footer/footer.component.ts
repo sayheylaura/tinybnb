b17e9d1d54e4c9c73cf56a1fbe251f1af054a0a0
sayheylaura <lauraferrandof@gmail.com>
2025-07-28 14:33:18 +0200

    feat: improve footer look and feel

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -8,28 +8,29 @@ import { GitHubIconComponent } from '../../../assets/icons/github.icon';
   standalone: true,
   imports: [RouterLink, GitHubIconComponent],
   template: `<footer class="footer">
-    <p data-testid="footer-copyright">
-      &copy; {{ currentYear }}
-      <a
-        routerLink="/"
-        class="footer-link"
-        aria-label="Go to homepage"
-        data-testid="footer-link"
-        >Tinybnb</a
-      >
-    </p>
-    <div class="footer-divider">|</div>
-    <p data-testid="footer-description">Your home away from home</p>
-    <div class="footer-divider">|</div>
-    <div class="footer-social-links">
-      <a
-        href="https://github.com/sayheylaura/tinybnb"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <app-github-icon color="rebeccapurple" />
-      </a>
+    <div class="footer-content">
+      <p data-testid="footer-copyright">
+        &copy; {{ currentYear }}
+        <a
+          routerLink="/"
+          class="footer-link"
+          aria-label="Go to homepage"
+          data-testid="footer-link"
+          >Tinybnb</a
+        >
+      </p>
+      <div class="footer-divider">|</div>
+      <div class="footer-social-links">
+        <a
+          href="https://github.com/sayheylaura/tinybnb"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <app-github-icon color="rebeccapurple" />
+        </a>
+      </div>
     </div>
+    <p data-testid="footer-description">Your home away from home</p>
   </footer>`,
   styleUrls: ['./footer.component.scss'],
 })
